c79a02c4bc42bb37fbcce5ada1e207e8e27fbea2
RaedRokh <raed.rokh@supcom.tn>
2023-08-17 16:09:44 +0100

    Add binary search function

--- array/main.cpp
+++ array/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 struct Array {
 int *A;
@@ -29,11 +28,30 @@ if (index>=0 && index<length){
 int lsearch(int key){
     for (int i=0; i<length ; i++){
         if (A[i]== key){
+            swap(A[i],A[0]);
             return i;
         }
     }
     return -1;
 };
+int bsearch(int key){
+    int high=length-1;
+    int low=0;
+    int mid;
+    while (high>=low){
+        mid=(low+high)/2;
+        if (key<A[mid]){
+            high=mid-1;
+        }
+        else if (key > A[mid]){
+            low=mid+1;
+        }
+        else{
+            return mid;}
+
+    }
+    return -1;
+}
 };
 
 void display(struct Array arr){
@@ -49,16 +67,9 @@ int main()
 {
     int *x=new int[3];
     x[0]=1;
-    x[1]=1;
-    x[2]=1;
-    struct Array arr ={x,5,3};
-    display(arr);
-    arr.Insert(1,2);
-    arr.Add(5);
-    display(arr);
-
-arr.Delete(0);
-display(arr);
-cout<<arr.lsearch(3);
+    x[1]=2;
+    x[2]=3;
+    struct Array arr ={x,4,5};
+    cout<<arr.bsearch(2);
     return 0;
 }
