c125f5cc421fba9a7ab71c51e1c811702e9b4f1e
RaedRokh <raed.rokh@supcom.tn>
2023-08-21 14:32:31 +0100

    New Permute string using Backtracking

--- Permutationstring.cpp
+++ Permutationstring.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 void permute(char s[]){
+int k,i;
+int A[strlen(s)]={0};
+if ()
 
 }
 int main(){
-char String[]={'a','b','c'};
+char String[]={'a','b','c','\0'};
 permute(String);
 return 0;}
