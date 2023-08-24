3b7181e94371691e2fa21d96c3f27a7387d6536f
RaedRokh <raed.rokh@supcom.tn>
2023-08-24 10:28:05 +0100

    Change from struct dmatrix to class & code refactor within dmatrix class

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,37 +1,43 @@
 #include <iostream>
 using namespace std;
-struct matrix{
-
-int A[10];
+class dmatrix{
+private:
+int *A;
 int n;
+public:
+dmatrix(int n){
+this->n=n;
+A=new int[n];}
 void Set (int i, int j, int x){
 if (i==j){
     A[i-1]=x;
     }
 }
-int Get(int i, int j, int x){
+int Get(int i, int j){
 if (i==j){
     return A[i-1];
 }
 else { return 0;}}
 void Display(){
 int i,j;
-for (i=0;i<n;i++){
-    for (j=0;j<n;j++){
+for (i=1;i<=n;i++){
+    for (j=1;j<=n;j++){
         if (i==j){
-            cout<<A[i-1]<<endl;
+            cout<<A[i-1];
         }
-        else {cout<<0<<endl;}
+        else {cout<<0;}
     }
+    cout<<endl;
 }
-}};
+}
+~dmatrix(){delete []A;}
+};
 int main(){
-matrix m;
-m.n=4;
-m.Set(1,1,5);
+dmatrix m(4);
+m.Set(1,1,1);
 m.Set(2,2,8);
 m.Set(3,3,9);
-m.Set(4,4,10);
+m.Set(4,4,3);
 m.Display();
 return 0;
 }
