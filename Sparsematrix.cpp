d17ed24605fff77d28818402fbfa237f2d81b480
RaedRokh <raed.rokh@supcom.tn>
2023-08-24 16:37:34 +0100

    Change constructor to create function, Add add function and fix minor bugs

--- Sparsematrix.cpp
+++ Sparsematrix.cpp
@@ -13,7 +13,7 @@ int n;
 int num;
 element *e;
 public:
-sparse(){
+void create(){
 cout<<"Enter dimensions & num of non zero elements"<<endl;
 cin>>n>>m>>num;
 e=new element [num];
@@ -23,7 +23,7 @@ for (int i=0;i<num;i++){
 }
 
 }
-Display(){
+void Display(){
     int i,j,k=0;
 for (i=0;i<n;i++){
     for (j=0;j<m;j++)
@@ -36,12 +36,55 @@ for (i=0;i<n;i++){
     cout<<endl;
 }
 }
+sparse * add (sparse *b){
+
+sparse *sum;
+sum=new sparse;
+sum->m=this->m;
+sum->n=this->n;
+sum->e=new element[this->num+b->num];
+int i=0,j=0,k=0;
+while (i<this->num && j<b->num){
+if (this->e[i].i<b->e[j].i){
+    sum->e[k++]=this->e[i++];
+}
+else if(this->e[i].i>b->e[j].i){
+sum->e[k++]=b->e[j++];
+}
+else {
+    if (this->e[i].j<b->e[j].j){
+        sum->e[k++]=this->e[i++];
+    }
+    else if (this->e[i].j>b->e[j].j){
+       sum->e[k++]=b->e[j++];
+    }
+    else {sum->e[k]=this->e[i++];
+        sum->e[k++].x+=b->e[j++].x;
+        }
+}
+}
+for (;i<this->num;i++){
+    sum->e[k++]=this->e[i];
+}
+for (;j<b->num;j++){
+    sum->e[k++]=this->e[j];
+}
+sum->num=k;
+return sum;
+}
 
 
 };
 
 int main(){
 sparse s;
+s.create();
 s.Display();
+sparse b;
+b.create();
+b.Display();
+cout<<"Result of addition is: "<<endl;
+sparse* c = s.add(&b);
+c->Display();
 
 return 0;}
