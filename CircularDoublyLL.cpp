0aee0dcdde225dee2fce22dcbd647ec9b83772aa
RaedRokh <raed.rokh@supcom.tn>
2023-08-30 10:53:02 +0100

    Add Delete function for Circular Doubly LL

--- CircularDoublyLL.cpp
+++ CircularDoublyLL.cpp
@@ -15,7 +15,7 @@ public:
     CDLL(int A[],int n);
     void Display();
     void Insert(int pos, int key);
-    void Delete(int pos);
+    int Delete(int pos);
 };
 
 CDLL::CDLL(int A[],int n){
@@ -68,9 +68,36 @@ p->next->prev=t;
 p->next=t;
 }
 }
+int CDLL::Delete(int pos){
+if (!head){
+    return -1;
+}
+int x;
+if (pos==0){
+        node *t=head;
+        x=t->data;
+        t->prev->next=t->next;
+        t->next->prev=t->prev;
+
+        head=t->next;
+        delete t;
+}
+else {
+    node *p=head;
+    for (int i=0;i<pos;i++){
+        p=p->next;
+    }
+    p->prev->next=p->next;
+    p->next->prev=p->prev;
+    x=p->data;
+    delete p;
+}
+
+}
+
 int main(){
 int A[]={1,3,5,2,4};
 CDLL *m=new CDLL(A,5);
-m->Insert(0,0);
+m->Delete(4);
 m->Display();
 return 0;}
