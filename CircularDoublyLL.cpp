eeb5b4f7666ea89f54a9e90344062e49c9bd6a19
RaedRokh <raed.rokh@supcom.tn>
2023-08-30 10:39:26 +0100

    Add Insert function for Circular Doubly LL

--- CircularDoublyLL.cpp
+++ CircularDoublyLL.cpp
@@ -12,7 +12,6 @@ class CDLL{
 private:
         node * head;
 public:
-
     CDLL(int A[],int n);
     void Display();
     void Insert(int pos, int key);
@@ -42,8 +41,36 @@ do {
     p=p->next;
 }while (p!=head);
 }
+void CDLL::Insert(int pos,int key){
+node *t=new node;
+t->data=key;
+if (!head){
+    t=head;
+    head->next=head;
+    head->prev=head;
+}
+if (pos==0){
 
+t->next=head;
+t->prev=head->prev;
+head->prev=t;
+head->prev->next=t;
+head=t;
+}
+else {
+node *p=head;
+for (int i=0;i<pos-1;i++){
+    p=p->next;
+}
+t->next=p->next;
+t->prev=p;
+p->next->prev=t;
+p->next=t;
+}
+}
 int main(){
 int A[]={1,3,5,2,4};
 CDLL *m=new CDLL(A,5);
+m->Insert(0,0);
+m->Display();
 return 0;}
