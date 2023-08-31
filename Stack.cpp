7e50a8ce96ab1df125bfc8e0297805ebd1f4d610
RaedRokh <raed.rokh@supcom.tn>
2023-08-31 17:36:06 +0100

    Add Stack class with basic methods: constructor,push,pop,peek,display

---
+++ Stack.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class Stack{
+private:
+    int Size;
+    int Top=-1;
+    int * st;
+    public:
+        Stack(int Size){
+            this->Size=Size;
+            st=new int[Size];
+        }
+        void push(int e);
+        void display();
+        int pop();
+        int isEmpty();
+        int isFull();
+        int Peek(int pos);
+
+};
+int Stack::isEmpty(){
+    return Top==-1;
+}
+int Stack::isFull(){
+    return Top==Size-1;
+}
+void Stack::push(int e){
+if (isFull()){
+    cout<<"Stack overflow"<<endl;
+    }
+    else {
+        Top++;
+        st[Top]=e;
+    }
+}
+int Stack::pop(){
+    int x=-1;
+if (isEmpty()){
+    cout<<"Stack underflow"<<endl;
+
+}
+else {
+    x=st[Top];
+    Top--;
+}
+return x;}
+int Stack::Peek(int pos){
+    int index=Top-pos+1;
+    if (index<0){
+        return -1;
+    }
+    else{
+        return st[index];
+    }
+}
+void Stack::display(){
+for (int i=0;i<=Top;i++){
+    cout<<st[i]<<endl;
+}
+}
+int main(){
+//int s;
+//cout<<"Enter stack size :"<<endl;
+//cin>>s;
+Stack *st=new Stack(5);
+st->push(1);
+st->push(2);
+st->push(3);
+st->push(4);
+st->push(5);
+st->display();
+
+
+return 0;
+}
