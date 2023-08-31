32e977fde6501a6bac161ec133b61a92fe439dd7
RaedRokh <raed.rokh@supcom.tn>
2023-08-31 19:26:51 +0100

    Add StackTop function for Stack

--- Stack.cpp
+++ Stack.cpp
@@ -1,76 +1,81 @@
 #include <iostream>
 using namespace std;
 
-class Stack{
-private:
+class Stack {
+  private:
     int Size;
     int Top=-1;
     int * st;
-    public:
-        Stack(int Size){
-            this->Size=Size;
-            st=new int[Size];
-        }
-        void push(int e);
-        void display();
-        int pop();
-        int isEmpty();
-        int isFull();
-        int Peek(int pos);
+  public:
+    Stack(int Size) {
+        this->Size=Size;
+        st=new int[Size];
+    }
+    void push(int e);
+    void display();
+    int pop();
+    int isEmpty();
+    int isFull();
+    int Peek(int pos);
+    int StackTop();
 
 };
-int Stack::isEmpty(){
+int Stack::isEmpty() {
     return Top==-1;
 }
-int Stack::isFull(){
+int Stack::isFull() {
     return Top==Size-1;
 }
-void Stack::push(int e){
-if (isFull()){
-    cout<<"Stack overflow"<<endl;
-    }
-    else {
+void Stack::push(int e) {
+    if (isFull()) {
+        cout<<"Stack overflow"<<endl;
+    } else {
         Top++;
         st[Top]=e;
     }
 }
-int Stack::pop(){
+int Stack::pop() {
     int x=-1;
-if (isEmpty()){
-    cout<<"Stack underflow"<<endl;
+    if (isEmpty()) {
+        cout<<"Stack underflow"<<endl;
 
-}
-else {
-    x=st[Top];
-    Top--;
-}
-return x;}
-int Stack::Peek(int pos){
-    int index=Top-pos+1;
-    if (index<0){
-        return -1;
+    } else {
+        x=st[Top];
+        Top--;
     }
-    else{
+    return x;
+}
+int Stack::Peek(int pos) {
+    int index=Top-pos+1;
+    if (index<0) {
+        return -1;
+    } else {
         return st[index];
     }
 }
-void Stack::display(){
-for (int i=0;i<=Top;i++){
-    cout<<st[i]<<endl;
+void Stack::display() {
+    for (int i=0; i<=Top; i++) {
+        cout<<st[i]<<endl;
+    }
 }
+int Stack::StackTop() {
+    if (isEmpty()) {
+        return -1;
+    } else {
+        return st[Top];
+    };
 }
-int main(){
+int main() {
 //int s;
 //cout<<"Enter stack size :"<<endl;
 //cin>>s;
-Stack *st=new Stack(5);
-st->push(1);
-st->push(2);
-st->push(3);
-st->push(4);
-st->push(5);
-st->display();
+    Stack *st=new Stack(5);
+    st->push(1);
+    st->push(2);
+    st->push(3);
+    cout<<st->StackTop()<<endl;
+    cout<<st->Peek(3);
 
 
-return 0;
+    return 0;
 }
