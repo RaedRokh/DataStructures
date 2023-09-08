d7fd2688133f409fd64f73cd23ac8b7add3ab6fa
raed rokh <raed.rokh@supcom.tn>
2023-09-08 17:11:16 +0100

    Switch Recursive binary search function to BST class

--- Binarysearchtree.cpp
+++ Binarysearchtree.cpp
@@ -4,7 +4,20 @@ using namespace Q;
 class bst:public tree {
 public:
     void Insert(int key);
+    Q::node* Rsearch(Q::node* p,int key);
 };
+Q::node* bst::Rsearch(Q::node* p,int key){
+    if (!p){
+        return 0;
+    }
+    if (p->data==key){
+        return p;
+    }
+    else if (p->data>key){
+        return Rsearch(p->lchild,key);
+    }
+    else {return Rsearch(p->rchild,key);}
+}
 void bst::Insert(int key) {
     node * t=root;
     node * r=root;
@@ -36,8 +49,12 @@ p->lchild=0;
 int main() {
     bst * t=new bst;
     t->Insert(10);
-
-
+  t->Insert(20);
+  t->Insert(30);
     t->inorderit(t->root);
+    if (t->Rsearch(t->root,40)){
+        cout<<"found"<<endl;}
+    else {cout<<"unfound"<<endl;}
+
     return 0;
 }
